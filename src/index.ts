ca118fc5df93bd87776fb9ad9bef0f9f58d7daaf
AlanNJ <alannpn75@gmail.com>
2023-04-10 23:20:14 +0530

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,9 @@ import { FileReader } from "./CSVfileReader";
 import { MatchReader } from "./MatchReader";
 import { Summary } from "./summary";
 
+// creating instance of matches and feeded into summary sub module
 let matches = new MatchReader(new FileReader("./2018-19.csv"));
 let summary = Summary.summery();
 
 matches.load();
 summary.buildAndPrint(matches.matches);
-
-// matches.map((item: string[]): void => {
-// 	console.log(dateConverter(item[1]));
-// });
-
-// dateConverter(date);
